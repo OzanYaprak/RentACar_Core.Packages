7962e87d1f6588c7b96b43203a2d670c76ad3a51
Ozan Yaprak <oznyprk@gmail.com>
2026-03-08 18:35:49 +0300

    Add MediatR transaction pipeline with marker interface
    Introduced ITransactionalRequest marker interface and TransactionScopeBehavior pipeline to enable automatic transaction handling for MediatR requests. Requests implementing ITransactionalRequest are now wrapped in a TransactionScope, ensuring transactional consistency and error handling.

---
+++ Core.Application/Pipelines/Transaction/TransactionScopeBehavior.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System.Transactions;
+
+namespace Core.Application.Pipelines.Transaction;
+
+public class TransactionScopeBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>, ITransactionalRequest
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        using TransactionScope transactionScope = new TransactionScope(asyncFlowOption: TransactionScopeAsyncFlowOption.Enabled);
+        {
+            TResponse response;
+            try
+            {
+                response = await next();
+                transactionScope.Complete();
+            }
+            catch (Exception ex)
+            {
+
+                transactionScope.Dispose();
+                throw new Exception($"An error occurred while processing the transaction. {ex.Message}", ex);
+            }
+            return response;
+        }
+    }
+}
